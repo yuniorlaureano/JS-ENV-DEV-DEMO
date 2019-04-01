9b43c236e89bd032beef1614b98c7d68a7fc2cb8
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2019-04-01 09:01:23 -0400

    Webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,26 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 var port = 3000;
 var app = express();
+const compiler = webpack(config);
 
-app.get('/', function(req, resp){
-  resp.sendFile(path.join(__dirname,'../src/index.html'));
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.get('/', function(req, resp) {
+    resp.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err){
-  if(err){
-    console.log(err);
-  }else{
-    open('http://localhost:' + port);
-  }
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
 });
